3e8bc7ef9d1405636f8a93956c5c8290bf7ba4f1
andrusha52 <andrushaes@gmail.com>
2020-11-18 00:13:35 +0200

    fix

--- android/app/src/main/java/com/finddifferences/MainApplication.java
+++ android/app/src/main/java/com/finddifferences/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.art.ARTPackage;
 
 import com.thebylito.navigationbarcolor.NavigationBarColorPackage;
 import com.facebook.react.ReactInstanceManager;
